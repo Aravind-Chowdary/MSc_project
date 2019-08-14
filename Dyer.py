b1dfe0515c27cb2f902000db1f7120968090e3b9
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-08-14 11:12:39 +0100

    Own paper code

---
+++ Dyer.py
@@ -0,0 +1,18 @@
+import math
+
+M=math.infi
+N=(math.infi)+1
+N>M
+m={0,M}
+c={0,N}
+a={input('Please enter the word' )}
+txt=m.issubset(a)
+def key_gen(s, k):
+    λ=0
+    λ in s
+    λ > 8 / 3l
+    set= {2^λ,2^λ+1}
+    k in set
+
+
+
