582541c5860cea26f61b76be896825ffd1aa1ae6
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-08-14 11:35:42 +0100

    Issues with installing pacakge pycrypto

--- Dyer.py
+++ Dyer.py
@@ -1,4 +1,5 @@
 import math
+import pycrypto
 
 M=math.infi
 N=(math.infi)+1
@@ -10,9 +11,11 @@ txt=m.issubset(a)
 def key_gen(s, k):
     λ=0
     λ in s
-    λ > 8 / 3l
+    λ > 8 /3*M
     set= {2^λ,2^λ+1}
     k in set
+    k > M^8/3
 
+def enc(k, r):
 
 
