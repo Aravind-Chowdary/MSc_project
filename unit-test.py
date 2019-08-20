9dde3d8d881d551af776e7aabaa3b70e6d51f85d
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-08-20 23:01:26 +0100

    Unit test implementation for james scheme

---
+++ unit-test.py
@@ -0,0 +1,32 @@
+import unittest
+import random
+
+
+from Dyer import key_gen, enc, dec
+
+
+class SimpleTestCase(unittest.TestCase):
+    k = key_gen(48)
+
+    def testConsistency(self):
+        m = random.randint(1, 65535)
+        c = enc(self.k, m)
+        d = dec(self.k, c)
+        assert d == m
+
+    def testOrderPreserving(self):
+        m1 = random.randint(1, 65535)
+        m2 = random.randint(1, 65535)
+        c1 = enc(self.k, m1)
+        c2 = enc(self.k, m2)
+        if m1 < m2:
+            assert c1 < c2
+        if m1 > m2:
+            assert c1 > c2
+        assert True
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
