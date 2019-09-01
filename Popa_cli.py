ebe6f7a64541dad1533aa5faa74319fdf332d3d0
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-09-01 11:59:17 +0100

    Changes in encryption method

--- Popa_cli.py
+++ Popa_cli.py
@@ -10,9 +10,9 @@ def init_vect():
     iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
 
 
-def enc(key, iv):
+def enc(key, iv, data):
     aes = AES.new(key, AES.MODE_CBC, iv)
-    data = 'hello world 1234'  # <- 16 bytes
+    # data = 'hello world 1234'   <- 16 bytes
     encd = aes.encrypt(data)
 
 
