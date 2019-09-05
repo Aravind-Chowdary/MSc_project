bef775bf1543a10a519cfc88f81d921784ce9018
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-09-05 12:41:43 +0100

    Changes in testorderpreserving method for assert compare

--- Unit_test_CLWW.py
+++ Unit_test_CLWW.py
@@ -5,23 +5,19 @@ from My_CLWW import *
 
 
 class SimpleTestCase(unittest.TestCase):
-    k = key_gen(48)
-
-    def testConsistency(self):
-        m = random.randint(1, 65535)
-        c = ore_enc(self.k, m)
-        d = ore_compare(self.k, c)
-        assert d == m
+    k = key_gen()
 
     def testOrderPreserving(self):
         m1 = random.randint(1, 65535)
         m2 = random.randint(1, 65535)
         c1 = ore_enc(self.k, m1)
-        c2 = ore_compare(self.k, m2)
+        c2 = ore_enc(self.k, m2)
         if m1 < m2:
-            assert c1 < c2
+            assert ore_compare(c1<c2)=-1
         if m1 > m2:
-            assert c1 > c2
+            assert ore_compare(c1<c2)=1
+        if m1==m2:
+            assert ore_compare(c1=c2)=0
         assert True
 
 
