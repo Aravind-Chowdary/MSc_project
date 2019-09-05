a286b0ba253b634b8384572c55f19160a880c885
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-09-05 12:44:03 +0100

    commiting the old changes

--- My_CLWW.py
+++ My_CLWW.py
@@ -1,28 +1,28 @@
-from random import randrange
-import pycrypto
+from Crypto.Random import random
+from Crypto.Hash import HMAC,SHA256
 
 
-def key_gen(p):
-    k=randrange()
+def key_gen():
+    k=random.get_random_bytes(64)
     return k
 
 
-def pre(sk, m):
-    h=Hmac.new(sk,m,digestmod=SHA256)
+def pre(k, m):
+    h=HMAC.new(k,m,digestmod=SHA256)
     return h.digestmod()
 
 
-def ore_enc(k, m):
+def ore_enc(k, m, h):
     ab = ""
     ct1 =()
     for i in m:
         ab += i
-        ct1 += str((f(ab[:-1], k) + int(ab[-1])) % 3)
+        ct1 += str((h(ab[:-1], k) + int(ab[-1])) % 3)
     return ct1
 
 
 def ore_compare(ct1, ct2, st):
-    if  ct1!=ct2:
+    if  ct1<ct2:
         return 0
     else :
         return 1
