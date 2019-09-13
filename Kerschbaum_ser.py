88dbbd003e3aa85d18195a2696297dbdcc09c96f
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-09-13 11:37:04 +0100

    Implementation of redblack tree

--- Kerschbaum_ser.py
+++ Kerschbaum_ser.py
@@ -105,3 +105,138 @@ class RedBlackTree:
                     self.left_rotate(z.p.p)
             i += 1
         self.root.color = 'black'
+
+    def transplant(self, u, v):
+        if u.p == self.NIL:
+            self.root = v
+        elif u == u.p.left:
+            u.p.left = v
+        else:
+            u.p.right = v
+        v.p = u.p
+
+    def remove(self, z):
+        if self.size == 0:
+            print("TreeError")
+            return
+        our_node = self.key_search(z)
+        self._remove(our_node)
+        self.size -= 1
+
+    def _remove(self, z):
+        y = z
+        original_color = y.color
+        if z.left == self.NIL:
+            x = z.right
+            self.transplant(z, z.right)
+        elif z.right == self.NIL:
+            x = z.left
+            self.transplant(z, z.right)
+        else:
+            y = self._min_node(z.right)
+            original_color = y.color
+            x = y.right
+            if y.p == z:
+                x.p = y
+            else:
+                self.transplant(y, y.right)
+                y.right = z.right
+                y.right.p = y
+            self.transplant(z, y)
+            y.left = z.left
+            y.left.p = y
+            y.color = z.color
+        if original_color == 'black':
+            self.rb_delete_fixup(x)
+
+    def rb_delete_fixup(self, x):
+        while x != self.root and x.color == 'black':
+            if x == x.p.left:
+                w = x.p.right
+                if w.color == 'red':
+                    w.color = 'black'
+                    x.p.color = 'red'
+                    self.left_rotate(x.p)
+                    w = x.p.right
+                if w.left.color == 'black' and w.right.color == 'black':
+                    w.color = 'red'
+                    x = x.p
+                else:
+                    if w.right.color == 'black':
+                        w.left.color = 'black'
+                        w.color = 'red'
+                        self.right_rotate(w)
+                        w = x.p.right
+                    w.color = x.p.color
+                    x.p.color = 'black'
+                    w.right.color = 'black'
+                    self.left_rotate(x.p)
+                    x = self.root
+            else:
+                w = x.p.left
+                if w.color == 'red':
+                    w.color = 'black'
+                    x.p.color = 'red'
+                    self.right_rotate(x.p)
+                    w = x.p.left
+                if w.right.color == 'black' and w.left.color == 'black':
+                    w.color = 'red'
+                    x = x.p
+                else:
+                    if w.left.color == 'black':
+                        w.right.color = 'black'
+                        w.color = 'red'
+                        self.left_rotate(w)
+                        w = x.p.left
+                    w.color = x.p.color
+                    x.p.color = 'black'
+                    w.left.color = 'black'
+                    self.right_rotate(x.p)
+                    x = self.root
+        x.color = 'black'
+
+    def search(self, x):
+        return self._search(self.root, x)
+
+    def _search(self, current_node, target):
+        if current_node == self.NIL:
+            return "NotFound"
+        elif target == current_node.key:
+            return "Found"
+        elif target < current_node.key:
+            return self._search(current_node.left, target)
+        else:
+            return self._search(current_node.right, target)
+
+    def key_search(self, target):
+        return self._key_search(self.root, target)
+
+    def _key_search(self, current_node, target):
+        if current_node == self.NIL:
+            return None
+        elif target == current_node.key:
+            return current_node
+        elif target < current_node.key:
+            return self._key_search(current_node.left, target)
+        else:
+            return self._key_search(current_node.right, target)
+
+    def maximum(self):
+        if self.size == 0:
+            return "Empty"
+        return self._maximum(self.root)
+
+    def _maximum(self, x):
+        while x.right != self.NIL:
+            x = x.right
+        return x.key
+
+    def minimum(self):
+        if self.size == 0:
+            return "Empty"
+        return self._minimum(self.root)
+
+    def _minimum(self, x):
+        while x.left != self.NIL:
+            x = x.left
+        return x.key
