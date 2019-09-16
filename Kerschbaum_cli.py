54c52c40a8ad64ed6ebf71ca4e10cbc7c45188eb
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-09-16 12:20:10 +0100

    Implementation of Kerschbaum client side encryption method

--- Kerschbaum_cli.py
+++ Kerschbaum_cli.py
@@ -1,4 +1,6 @@
 import Crypto.Random
+from Crypto.Hash import HMAC,SHA256
+import math
 from Kerschbaum_ser import *
 
 class Node:
@@ -22,12 +24,24 @@ def key_gen():
     k=Crypto.Random.random.randint(0, 1)
     return k
 
-def enc (x,t,min,max):
-    if (t.right!= None):
-        return enc(x,t,max,min)
-    elif (max-t.x<2):
-        return rebalance (x,-1,n)
-        t.right =HMAC.new(x,t.l,)
+def enc (x,t,min,max,k):
+    r=math.log(2,n)
+    p=2**r
+    if (x>t.x or k==1):
+
+        if (t.right!= None):
+            return enc(x,t,max,min)
+        elif (max-t.x<2):
+            return rebalance (x,-1,n)
+        t.right =HMAC.new(x,t.l,max-t.l/2,digestmod=SHA256)
+        return t.right.l
+    if (x<t.l or k==0):
+        if (t.left!= None):
+            return enc(x,t.left,min,t.l)
+        elif (t.l-min<2):
+            return rebalance(x,-1,p)
+        t.left=Tree(x,min+(t.l-min/2))
+
 
 def rebalance (x,min,max):
 
