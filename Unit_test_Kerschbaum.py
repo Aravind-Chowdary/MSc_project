d9671a52275d4d40ff1e11376384cd47a50d715c
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-09-23 11:41:11 +0100

    Unit test for Kershbaum

---
+++ Unit_test_Kerschbaum.py
@@ -0,0 +1,18 @@
+import unittest
+import random
+
+from Kerschbaum_cli import *
+
+
+class SimpleTestCase(unittest.TestCase):
+    y = enc()
+
+
+    def testConsistency(self):
+        k=random.randint(0,1)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
