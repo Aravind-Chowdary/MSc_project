1d6364eadc3912d4a03875e3ad3e35e4b1338afc
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-10-05 00:07:44 +0100

    Another Implementation of Kerschbaum schema intial the basic methods.

---
+++ Kerschbaum.py
@@ -0,0 +1,29 @@
+import Crypto.Random
+import math
+
+MAX = None
+n = None
+T = None
+
+
+class Tree:
+    left = None
+    right = None
+    plain = None
+    cipher = None
+
+    def __init__(self, plain, cipher):
+        self.left = None
+        self.right = None
+        self.plain = plain
+        self.cipher = cipher
+
+
+def get(t, plain):
+
+
+def get_all_plaintexts(l, t):
+
+
+def calc_max(l):
+
