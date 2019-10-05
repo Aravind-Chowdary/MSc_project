f480b84fe6a1b2f455eb5ad3e01176aad92294d2
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-10-05 10:19:08 +0100

    Implementation of the coin principle

--- Kerschbaum.py
+++ Kerschbaum.py
@@ -20,7 +20,26 @@ class Tree:
 
 
 def get(t, plain):
-
+    if t.plain == plain:
+        return t
+    else:
+        l = None
+        r = None
+        if t.left is not None:
+            l = t.get(t.left, plain)
+        if t.right is not None:
+            r = t.get(t.right, plain)
+        if l is not None and r is not None:
+            coin = Crypto.Random.random.randint(0, 1)
+            if coin == 0:
+                return l
+            else:
+                return r
+        if l is not None:
+            return l
+        if r is not None:
+            return r
+        return None
 
 def get_all_plaintexts(l, t):
 
